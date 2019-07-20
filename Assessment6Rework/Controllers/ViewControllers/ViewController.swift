155aa6dcbea37feedb8705842edc84674514c331
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-20 11:36:46 -0600

    Initial Commit

---
+++ Assessment6Rework/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessment6Rework
+//
+//  Created by Timothy Rosenvall on 7/20/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
